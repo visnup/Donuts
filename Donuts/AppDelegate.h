019eeed6fffe476f011de2d46e3ee73c34b9520c
Visnu Pitiyanuvath <visnupx@gmail.com>
2013-01-07 20:32:53 -0800

    Initial Commit

---
+++ Donuts/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Donuts
+//
+//  Created by Visnu on 1/7/13.
+//  Copyright (c) 2013 Visnu Pitiyanuvath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Donuts/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Donuts
+//
+//  Created by Visnu on 1/7/13.
+//  Copyright (c) 2013 Visnu Pitiyanuvath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
